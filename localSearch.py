0a54d47e3cbd958ad08beea5d6b97c2acd9322d6
sami-bre <samuelbirhanu121@gmail.com>
2023-05-11 10:14:36 +0300

    created a 2nd geneticAlgorithm method in the LocalSearch class specifically for the tsp problem. I also created two helper methods: _reproduceTSP and _mutateTsp

--- localSearch.py
+++ localSearch.py
@@ -72,13 +72,13 @@ class LocalSearch:
 
         return parents
     
-    def _reproduce(self,px,py):
+    def _reproduceKnapsack(self,px,py):
         lp = len(px)-3 
         cp = random.randint(2,lp)
 
         return px[0:cp] + py[cp:]
 
-    def _mutate(self,child):
+    def _mutateKnapsack(self,child):
     
         possibleVals = [x for x in range(0, len(child))]
 
@@ -92,7 +92,7 @@ class LocalSearch:
 
 
 
-    def geneticAlgorithm(self,population,problem):
+    def geneticAlgorithmKnapsack(self,population,problem):
 
         mutateRate = 0.1
 
@@ -114,14 +114,77 @@ class LocalSearch:
 
             for i in range(0,len(population)):
                 parentX,parentY = self._selectParents(populationWithFitness)
-                child  = self._reproduce(parentX ,parentY )
+                child  = self._reproduceKnapsack(parentX ,parentY )
 
                 if random.random() < mutateRate:
-                    child = self._mutate(child)
+                    child = self._mutateKnapsack(child)
 
                 newPopulation.append(child)
             
             population = newPopulation
         
             generation+=1
-    
+
+    def _reproduceTsp(self, parent1, parent2):
+        # Generate two random crossover points
+        crossover_points = sorted(random.sample(range(len(parent1)), 2))
+
+        # Extract the sub-tours to be crossed over from each parent
+        sub_tour_1 = parent1[crossover_points[0]:crossover_points[1]]
+        sub_tour_2 = parent2[crossover_points[0]:crossover_points[1]]
+
+        # Generate a child using order crossover
+        child = [-1] * len(parent1)
+        child[crossover_points[0]:crossover_points[1]] = sub_tour_1
+        remaining_indices = [i for i in range(len(parent1)) if parent1[i] not in sub_tour_1]
+        remaining_values = [v for v in parent2 if v not in sub_tour_1]
+        for i, value in zip(remaining_indices, remaining_values):
+            # what does the zip function do?
+            # a: it creates a list of tuples
+            # a: each tuple contains one index and one value
+            # a: the index is from the list of indices that are not in the sub-tour
+            # a: the value is from the list of values that are not in the sub-tour
+            if child[i] == -1:
+                child[i] = value
+
+        return child
+
+
+    def _mutateTsp(self, individual):
+        # Generate two random mutation points
+        mutation_points = random.sample(range(len(individual)), 2)
+
+        # Swap the cities at the mutation points
+        individual[mutation_points[0]], individual[mutation_points[1]] = \
+            individual[mutation_points[1]], individual[mutation_points[0]]
+
+        return individual
+
+    def geneticAlgorithmTsp(self, population, problem):
+        mutate_rate = 0.1
+        generation = 0
+
+        while True:
+            if generation == 400:
+                population.sort(key=lambda s: problem.fitness(s))
+                return population[-1], problem.fitness(population[-1])
+
+            population_with_fitness = []
+
+            new_population = []
+
+            for state in population:
+                population_with_fitness.append((state, problem.fitness(state)))
+
+            for i in range(0, len(population)):
+                parent_x, parent_y = self._selectParents(population_with_fitness)
+                child = self._reproduceTsp(parent_x, parent_y)
+
+                if random.random() < mutate_rate:
+                    child = self._mutateTsp(child)
+
+                new_population.append(child)
+
+            population = new_population
+
+            generation += 1
